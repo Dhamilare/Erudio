0a363329ea7ad04c37ae71f43fb9fe230b2e3236
Dhamilare <samuelholuwatosin@gmail.com>
2025-10-16 23:58:37 +0100

    recent changes

--- Erudio/settings.py
+++ Erudio/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
 import dj_database_url
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
